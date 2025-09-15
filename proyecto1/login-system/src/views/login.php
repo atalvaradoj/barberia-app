201fa405f37c9da542e59c6cba2b03cdabb8f1e2
Jerald Alvarado Vanegas <65480591+atalvaradoj@users.noreply.github.com>
2025-09-14 20:33:07 -0600

    cc

--- proyecto1/login-system/src/views/login.php
+++ proyecto1/login-system/src/views/login.php
@@ -5,7 +5,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Inicio de Sesión - Barbería</title>
+    <title>Inicio de Sesión - Barbería Hola</title>
     <link rel="stylesheet" href="../../public/css/styles.css">
 </head>
 <body>
